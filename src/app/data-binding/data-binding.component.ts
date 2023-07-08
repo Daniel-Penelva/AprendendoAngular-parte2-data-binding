412b9a0a61dc8e01aa8e1fd12ac3fc0439bfe9f8
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-08 04:03:20 -0300

    Property Binding e Interpolação

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss'],
+})
+export class DataBindingComponent implements OnInit {
+  url: string = 'https://www.google.com/';
+
+  // Não foi tipificado o tipo, pois o TypeScript sabe que é um String, já que está entre aspas simples.
+  urlImage = 'https://brixtonforged.com/wp-content/uploads/2023/03/miami-blue-porsche-991-gt3rs-wheels-brixton-forged-r11-r-satin-black.jpg';
+
+  cursoAngular: boolean = true;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
