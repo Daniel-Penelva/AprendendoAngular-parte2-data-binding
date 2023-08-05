05b1272602d07659a5ea40a3bb7896d42a4fb410
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-04 23:06:33 -0300

    Ciclo de vida do Componente - parte2

--- src/app/ciclo/ciclo.component.ts
+++ src/app/ciclo/ciclo.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, 
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
   styleUrls: ['./ciclo.component.scss']
 })
-export class CicloComponent implements OnInit {
+export class CicloComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, 
+AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() {}
+  // Para não ficar repetindo console.log();
+  private log(hook: string){
+    console.log(hook);
+  }
+  
+  constructor() {
+    this.log('constructor')
+   }
 
   ngOnInit(): void {
+    this.log('ngOnInit');
+  }
+
+  ngOnChanges(){
+    this.log('ngOnChanges');
+  }
+
+  ngDoCheck(){
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(){
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    this.log('AfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    this.log('ngAfterViewChecked');
+  }
+
+  // OnDestroy só irá aparecer no console quando não estiver mais na memória do browser
+  ngOnDestroy(){
+    this.log('ngOnDestroy');
   }
 
 }
